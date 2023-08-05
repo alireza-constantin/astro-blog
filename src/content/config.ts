456663ac17f6dff9b3d2df199ae6ddd1800c5f6e
alireza <alirezasoheili1224@yahoo.com>
2023-08-05 14:37:39 +0330

    add introduction

---
+++ src/content/config.ts
@@ -0,0 +1,10 @@
+// import { defineCollection, z } from "astro:content";
+// // 2. Define your collection(s)
+// const blogCollection = defineCollection({
+// 	type: "content",
+//     schema: (context) => 
+//         z.object({
+//             title: z.string().max(60)
+//         })
+    
+// });
