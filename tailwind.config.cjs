226d731965c52f210b002892c3ca0cab4ab5f861
alireza <alirezasoheili8080@yahoo.com>
2023-08-06 00:19:55 +0330

    add appear animation to wizard icon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,md,mdx,ts,tsx}"],
-    darkMode: "class",
-    theme: {
-        extend: {
-            colors: {
-                bgColor: "var(--theme-bg)",
-                textColor: "var(--theme-text)",
-                link: "var(--theme-link)",
-                accent: "var(--theme-accent)",
-                "accent-2": "var(--theme-accent-2)",
-            },
-            // fontFamily: {
-            //     sans: [...fontFamily.sans],
-            //     serif: [...fontFamily.serif],
-            // },
-        },
-    },
-    plugins: [],
-}
+	content: ["./src/**/*.{astro,html,js,md,mdx,ts,tsx}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				bgColor: "var(--theme-bg)",
+				textColor: "var(--theme-text)",
+				link: "var(--theme-link)",
+				accent: "var(--theme-accent)",
+				"accent-2": "var(--theme-accent-2)",
+			},
+			animation: {
+				appear: "appear 2s ease-in forwards",
+			},
+			keyframes: {
+				appear: {
+					"0%": { opacity: "0"},
+					"100%": { opacity: "1" },
+				},
+			},
+			// fontFamily: {
+			//     sans: [...fontFamily.sans],
+			//     serif: [...fontFamily.serif],
+			// },
+		},
+	},
+	plugins: [],
+};
