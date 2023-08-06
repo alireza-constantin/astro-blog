493763c0a591b37456feca4ed5ef0e64c658ca36
alireza <alirezasoheili8080@yahoo.com>
2023-08-06 14:19:43 +0330

    add footer and about page

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,18 @@
 export const LIGHT_THEME_COLOR = "#fafafa";
 export const DARK_THEME_COLOR = "#1d1f21";
-export const HOME_MAX_POST= 6
+export const HOME_MAX_POST= 6;
+export const MENU_LINKS = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+];
+
