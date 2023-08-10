c80f286e708334a6af6e3cf9514292392a5bfbb5
alireza <alirezasoheili8080@yahoo.com>
2023-08-10 15:02:28 +0330

    add post hero and post layout for blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,10 @@
 import { defineCollection, z } from "astro:content";
+import { postSchema } from "src/types";
 // 2. Define your collection(s)
 const post = defineCollection({
 	type: "content",
     schema: (_) => 
-        z.object({
-            title: z.string().max(60),
-            description: z.string().min(50).max(200),
-            publishDate: z.string().transform((str) => new Date(str))
-        })
+        postSchema
     
 });
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { z } from "astro/zod";
+
 export interface CustomThemeEvent extends CustomEvent {
 	detail: {
 		theme: string;
@@ -16,3 +18,11 @@ export type siteMeta = {
 	title: string,
 	description?: string,
 }
+
+export const postSchema = z.object({
+	title: z.string().max(60),
+	description: z.string().min(50).max(200),
+	publishDate: z.string().transform((str) => new Date(str)),
+});
+
+export type Post = z.infer<typeof postSchema>
