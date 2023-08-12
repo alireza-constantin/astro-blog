03c6fb0efce8bbea94321244380e8298d9abd3db
alireza <alirezasoheili8080@yahoo.com>
2023-08-12 20:53:23 +0330

    add prefetch to links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch"
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: true,
     configFile: './tailwind.config.cjs'
-  })]
+  }),
+  prefetch()]
 });
