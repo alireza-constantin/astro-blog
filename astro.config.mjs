e819c153bb0156fc562ed2b53bc1ef05c97ef75f
alireza <alirezasoheili8080@yahoo.com>
2023-08-13 15:40:38 +0330

    changed some configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,31 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  // change it with actual site url in prod
-  site: "http://localhost:3000",
-  markdown: {
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
-  integrations: [tailwind({
-    applyBaseStyles: true,
-    configFile: './tailwind.config.cjs'
-  }), prefetch(), mdx()],
-  experimental: {
-    assets: true
-  }
+	// change it with actual site url in prod
+	site: "http://localhost:3000",
+	markdown: {
+		shikiConfig: {
+			theme: "material-theme-palenight",
+			wrap: true,
+		},
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: true,
+			configFile: "./tailwind.config.cjs",
+		}),
+		prefetch(),
+		mdx(),
+	],
+	experimental: {
+		assets: true,
+	},
+	compressHTML: true,
+	vite: {
+		optimizeDeps: true,
+	},
 });
