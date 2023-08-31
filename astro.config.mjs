a023e31b39d680eb77139e355d87aec15fa648e0
alireza <alirezasoheili8080@yahoo.com>
2023-08-31 14:50:11 +0330

    updated astro to version 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,39 +2,28 @@ import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+
 
 // https://astro.build/config
 export default defineConfig({
-	// change it with actual site url in prod
-	site: "https://alirezasoh.vercel.app",
-	markdown: {
-		shikiConfig: {
-			theme: "material-theme-palenight",
-			wrap: true,
-		},
-		remarkPlugins: [remarkReadingTime],
-	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-			// configFile: "./tailwind.config.cjs",
-		}),
-		prefetch(),
-		mdx(),
-		sitemap()
-	],
-	image: {
-		service: sharpImageService(),
-	},
-	experimental: {
-		assets: true,
-	},
-	compressHTML: true,
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"]
-		}
-	}
+  // change it with actual site url in prod
+  site: "https://alirezasoh.vercel.app",
+  markdown: {
+    shikiConfig: {
+      theme: "material-theme-palenight",
+      wrap: true
+    },
+    remarkPlugins: [remarkReadingTime]
+  },
+  integrations: [tailwind({
+    applyBaseStyles: false
+    // configFile: "./tailwind.config.cjs",
+  }), prefetch(), mdx(), sitemap()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
